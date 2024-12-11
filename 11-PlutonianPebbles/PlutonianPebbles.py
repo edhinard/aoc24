0b965b479ef172d9bf5972f5029ab91d0d74fb66
edhinard <edouard.hinard@laposte.net>
2024-12-11 18:27:59 +0100

    11**

--- 11-PlutonianPebbles/PlutonianPebbles.py
+++ 11-PlutonianPebbles/PlutonianPebbles.py
@@ -3,47 +3,33 @@
 import aoc
 
 if aoc.part == "one":
-    stones = [*next(aoc.Input(split=" "))]
-    for _ in range(25):
-        newstones = []
-        for stone in stones:
-            if stone == "0":
-                newstones.append("1")
-            elif len(stone) % 2 == 0:
-                half = len(stone)//2
-                if half > 1:
-                    alldigit = False
-                a = stone[:half]
-                newstones.append(a)
-                b = stone[half:].lstrip("0") or "0"
-                newstones.append(b)
-            else:
-                newstones.append(str(int(stone) * 2024))
-        stones = newstones
-    print(len(stones))
+    numblinks = 25
+
 
 if aoc.part == "two":
-    stones = [*next(aoc.Input(split=" "))]
-    def blink75(stones):
-        for _ in range(75):
-            print(_, len(stones))
-            newstones = []
-            for stone in stones:
-                if stone == "0":
-                    newstones.append("1")
-                elif len(stone) % 2 == 0:
-                    half = len(stone)//2
-                    if half > 1:
-                        alldigit = False
-                    a = stone[:half]
-                    newstones.append(a)
-                    b = stone[half:].lstrip("0") or "0"
-                    newstones.append(b)
-                else:
-                    newstones.append(str(int(stone) * 2024))
-            stones = [s for s in newstones if len(s)>1]
-            if not stones:
-                break
-    for stone in stones:
-        print()
-        blink75([stone])
+    numblinks = 75
+
+
+stones = {s:1 for s in next(aoc.Input(split=" "))}
+for _ in range(numblinks):
+    newstones = {}
+    for stone, count in stones.items():
+        if stone == "0":
+            newstones.setdefault("1", 0)
+            newstones["1"] += count
+        elif len(stone) % 2 == 0:
+            half = len(stone)//2
+            if half > 1:
+                alldigit = False
+            a = stone[:half]
+            newstones.setdefault(a, 0)
+            newstones[a] += count
+            b = stone[half:].lstrip("0") or "0"
+            newstones.setdefault(b, 0)
+            newstones[b] += count
+        else:
+            s = str(int(stone) * 2024)
+            newstones.setdefault(s, 0)
+            newstones[s] += count
+    stones = newstones
+print(sum(stones.values()))
