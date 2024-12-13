04a176f4757e9692ab38f0d5f0f0166b6265064b
edhinard <edouard.hinard@laposte.net>
2024-12-13 10:43:14 +0100

    13**

--- 13-ClawContraption/ClawContraption.py
+++ 13-ClawContraption/ClawContraption.py
@@ -1,48 +1,30 @@
 #! /usr/bin/env python3
 
-import aoc
 import re
 
+import aoc
 
-if aoc.part == "one":
-
-    def press(ax,ay,bx,by,px,py):
-        for nb in range(min(100, px//bx), -1, -1):
-            for na in range(0, min(100,px//ax)+1, 1):
-                if px == nb*bx + na*ax and py == nb*by + na*ay:
-                    return 3*na + nb
-        return 0
-
-
-        
-    res = 0
-    for machine in aoc.Input(split=re.compile(r".*?(?P<x>\d+).*?(?P<y>\d+)"), groupby="paragraph"):
-        (ax,ay),(bx,by),(px,py) = [(int(x), int(y)) for x,y in machine]
-        c = press(ax,ay,bx,by,px,py)
-        res+=c
-        print(c)
-    print(res)
-
-
-if aoc.part == "two":
-    def press(ax,ay,bx,by,px,py):
-        px += 10000000000000
-        py += 10000000000000
-        na = (by*px-bx*py)/(by*ax-bx*ay)
-        nb = (ay*px-ax*py)/(ay*bx-ax*by)
-        if na == int(na) and nb == int(nb):
-            return 3*int(na) + int(nb)
-        return 0
-
-
-        
-    res = 0
-    for machine in aoc.Input(split=re.compile(r".*?(?P<x>\d+).*?(?P<y>\d+)"), groupby="paragraph"):
-        (ax,ay),(bx,by),(px,py) = [(int(x), int(y)) for x,y in machine]
-        c = press(ax,ay,bx,by,px,py)
-        res+=c
-        print(c)
-    print(res)
-
-
-#!415714979121903984
+# The instructions can be understood as:
+# find, if they exist, the two integers na and nb so that
+#      ->      ->   ->
+#   na.A  + nb.B  = P
+# with the two variants:
+#  - part 1:
+#    na and nb should be no more than 100
+#  - part 2:
+#    ->                         ->  ->
+#    P is added 10000000000000.(i + j)
+tokens = 0
+for (Ax,Ay),(Bx,By),(Px,Py) in aoc.Input(split=re.compile(r"(\d+).*?(\d+)"), convert=int, groupby="paragraph"):
+    if aoc.part == "two":
+        Px += 10000000000000  # noqa: PLW2901
+        Py += 10000000000000  # noqa: PLW2901
+    # finding
+    a = (By*Px - Bx*Py) / (By*Ax - Bx*Ay)
+    b = (Ay*Px - Ax*Py) / (Ay*Bx - Ax*By)
+    if not (((na:=int(a)) == a) and ((nb:=int(b)) == b)):
+        continue
+    if aoc.part == "one" and (na > 100 or nb > 100):  # noqa: PLR2004
+        continue
+    tokens += 3*na + nb
+print(tokens)
