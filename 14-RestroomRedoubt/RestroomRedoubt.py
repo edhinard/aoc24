21e371f6608833e6129a4ef7cc090f7f9e37c3d8
edhinard <edouard.hinard@laposte.net>
2024-12-18 15:35:59 +0100

    14**

--- 14-RestroomRedoubt/RestroomRedoubt.py
+++ 14-RestroomRedoubt/RestroomRedoubt.py
@@ -6,26 +6,25 @@ import aoc
 from PIL import Image, ImageDraw
 
 WIDTH,HEIGHT = (11,7) if aoc.test else (101,103)
-robots = [[px,py,vx,vy] for px,py,vx,vy in aoc.Input(split=re.compile(r"(-?\d+),(-?\d+).*?(-?\d+),(-?\d+)"),
-                                                     convert=int)]
 
-def move():
-    for robot in robots:
-        px,py,vx,vy = robot
-        robot[0] = (px + vx) % WIDTH
-        robot[1] = (py + vy) % HEIGHT
-
-def count(x,y):
-    count = 0
-    for px,py,_,_ in robots:
-         if px==x and py==y:
-             count += 1
-    return count
+robotre = re.compile(r"(-?\d+),(-?\d+).*?(-?\d+),(-?\d+)")
+robots = [[px,py,vx,vy] for px,py,vx,vy in aoc.Input(split=robotre, convert=int)]
 
 
 if aoc.part == "one":
-    for _ in range(100):
-        move()
+    # Move the robots 100 times each
+    for robot in robots:
+        px,py,vx,vy = robot
+        robot[0] = (px + 100 * vx) % WIDTH
+        robot[1] = (py + 100 * vy) % HEIGHT
+
+    # Count the number of robots in each quadrant
+    def count(x,y):
+        count = 0
+        for px,py,_,_ in robots:
+             if px==x and py==y:
+                 count += 1
+        return count
 
     A,B,C,D = 0,0,0,0
     for x in range(WIDTH // 2):
@@ -38,11 +37,18 @@ if aoc.part == "one":
 
 
 if aoc.part == "two":
+    def move():
+        for robot in robots:
+            px,py,vx,vy = robot
+            robot[0] = (px + vx) % WIDTH
+            robot[1] = (py + vy) % HEIGHT
+
     NUM_COL = 20
     NUM_ROW = 10
     imagenum = 0
     nummove = 0
     while nummove < WIDTH*HEIGHT:
+        print(f"drawing moves {nummove} to {min(nummove + NUM_ROW*NUM_COL - 1, WIDTH*HEIGHT)}")
         backgroundcolor = "white"
         im = Image.new("RGB", (NUM_COL*(WIDTH+3),NUM_ROW*(HEIGHT+3)), color=backgroundcolor)
         draw = ImageDraw.Draw(im)
@@ -58,12 +64,11 @@ if aoc.part == "two":
                 draw.line((left, bottom, right, bottom), fill="black")
                 draw.line((left, top, left, bottom), fill="black")
                 draw.line((right, top, right, bottom), fill="black")
-                for x in range(WIDTH):
-                    for y in range(HEIGHT):
-                        if count(x, y):
-                            draw.point((left + 1 + x, top + 1 + y), "red")
+                for x,y,_,_ in robots:
+                    draw.point((left + 1 + x, top + 1 + y), "red")
                 draw.text((left + 2, top+1), f"{nummove}", fill="blue")
                 move()
                 nummove += 1
         im.save(f"im{imagenum:02}.png")
+        print(f" --> im{imagenum:02}.png\n")
         imagenum += 1
