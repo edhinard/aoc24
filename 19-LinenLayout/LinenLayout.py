04ec19d24c8c5068de08d500da79b4e2702d0d6c
edhinard <edouard.hinard@laposte.net>
2024-12-19 18:54:32 +0100

    19**

--- 19-LinenLayout/LinenLayout.py
+++ 19-LinenLayout/LinenLayout.py
@@ -2,33 +2,30 @@
 
 import aoc
 
+lines = aoc.Input().read().splitlines()
+patterns = {}
+for p in lines.pop(0).split(", "):
+    patterns.setdefault(p[0], []).append(p)
+lines.pop(0)
+towels = list(lines)
+
+counts = {}
+def possible(towel, count=0):
+    if not towel:
+        return 1
+    if towel in counts:
+        return counts[towel]
+    counts[towel] = 0
+    p = towel[0]
+    for pattern in patterns.get(p, []):
+        if towel.startswith(pattern):
+            counts[towel] += possible(towel[len(pattern):], count)
+    return counts[towel]
+
+
 if aoc.part == "one":
-    input = aoc.Input().read().splitlines()
-    patterns = {}
-    for p in input.pop(0).split(", "):
-        patterns.setdefault(p[0], []).append(p)
-    input.pop(0)
-    
-    impossibles = set()
-
-    def possible(towel):
-        if not towel:
-            return True
-        if towel in impossibles:
-            return False
-        p = towel[0]
-        for pattern in patterns.get(p, []):
-            if towel.startswith(pattern) and possible(towel[len(pattern):]):
-                return True
-        impossibles.add(towel)
-        return False
-
-    count = 0
-    for i, towel in enumerate(input):
-        if possible(towel):
-            count += 1
-    print(count)
+    print(sum(1 for towel in towels if possible(towel)))
 
 
 if aoc.part == "two":
-    pass
+    print(sum(possible(towel) for towel in towels))
