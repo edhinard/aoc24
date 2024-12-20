dbf885a68426e3334d7effd1932554dedbe55146
edhinard <edouard.hinard@laposte.net>
2024-12-20 15:29:49 +0100

    16**

--- 16-ReindeerMaze/ReindeerMaze.py
+++ 16-ReindeerMaze/ReindeerMaze.py
@@ -1,153 +1,87 @@
 #! /usr/bin/env python3
 
-import sys
+import functools
 
 import aoc
 
 
+def findbestpaths(maze, startrow, startcol, endrow, endcol):
+    EAST = 0
+    SOUTH = 1
+    WEST = 2
+    NORTH = 3
+    directions = {EAST: (0,+1), SOUTH:(+1,0), WEST:(0,-1), NORTH:(-1, 0)}
+
+    # The nodes of the graph
+    #  keys are (row, col, direction) -- position in the maze
+    #  values are (score, tiles) -- score and visited tiles up to that position
+    nodes = {(startrow, startcol, EAST): (0, {(startrow, startcol)})}
+
+    # The working queue is a set of positions initialized with: start position, eastwards
+    queue = {(startrow, startcol, EAST)}
+
+    # A Dijkstra algorithm to find minimum score for each nodes
+    while queue:
+        #  Poping out the lowest score position from queue
+        score, position = min((nodes[pos][0], pos) for pos in queue)
+        queue.remove(position)
+        row, col, direction = position
+        score, tiles = nodes[position]
+
+        # Evaluate the three next nodes from here (new position, new score, new visited tiles, nextposition):
+        #  forward: one tile forward increase the score of 1
+        #  right and left: stay on the same tile but change direction and increase the score of 1000
+        dr, dc = directions[direction]
+        forward = ((row + dr, col + dc, direction), score + 1, tiles | {(row + dr, col + dc)}, row + dr, col + dc)
+        rightdirection = (direction + 1) % len(directions)
+        dr, dc = directions[rightdirection]
+        right = ((row, col, rightdirection), score + 1000, set(tiles), row + dr, col + dc)
+        leftdirection = (direction - 1) % len(directions)
+        dr, dc = directions[leftdirection]
+        left  = ((row, col, leftdirection), score + 1000, set(tiles), row + dr, col + dc)
+
+        # Loop on those 3 possible next nodes
+        #  * filter out nodes that:
+        #    - are not in the graph (walls)
+        #    - are already visited with a better score
+        #  * update queue and nodes
+        for position, score, tiles, nextrow, nextcol in (forward, right, left):
+            row, col, direction = position
+            if maze[row][col] == "#" or maze[nextrow][nextcol] == "#":
+                continue
+            if position in nodes:
+                previousscore, previoustiles = nodes[position]
+                if previousscore < score:
+                    continue
+                if previousscore == score:
+                    tiles |= previoustiles  # noqa: PLW2901
+            nodes[position] = (score, tiles)
+            queue.add(position)
+
+    # Collect the best paths at position E
+    endnodes = [nodes[(endrow, endcol, direction)] for direction in range(4) if (endrow, endcol, direction) in nodes]
+    endnodes.sort()
+    bestscore = endnodes[0][0]
+    besttiles = (tiles for score, tiles in endnodes if score == bestscore)
+    return bestscore, functools.reduce(set.union, besttiles)
+
+
+maze = []
+for row,line in enumerate(aoc.Input(split="")):
+    if "S" in line:
+        startrow = row
+        startcol = line.index("S")
+        line[startcol] = "."
+    if "E" in line:
+        endrow = row
+        endcol = line.index("E")
+        line[endcol] = "."
+    maze.append(line)
+
+bestscore, besttiles = findbestpaths(maze, startrow, startcol, endrow, endcol)
+
 
 if aoc.part == "one":
-    maze = []
-    for row,line in enumerate(aoc.Input(split="")):
-        if "S" in line:
-            srow = row
-            scol = line.index("S")
-            line[scol] = "."
-        maze.append(line)
-
-    EAST = 0
-    SOUTH = 1
-    WEST = 2
-    NORTH = 3
-    directions = [(0,+1), (+1,0), (0,-1), (-1, 0)]
-
-    def extractmin(queue):
-        score = sys.maxsize
-        position = None
-        for k, v in queue.items():
-            if v < score:
-                score = v
-                position = k
-        score = queue.pop(position)
-        row, col, direction = position
-        return score, row, col, direction
-
-    pastpositions = set()
-    def addinqueue(queue, score, row, col ,direction):
-        global pastpositions
-        position = (row, col, direction)
-        if position in pastpositions:
-            return
-        pastpositions.add(position)
-        position = (row, col ,direction)
-        previousscore = queue.get(position, sys.maxsize)
-        queue[position] = min(score, previousscore)
-
-    queue = {(srow, scol, EAST): 0}
-    bestscore = sys.maxsize
-    while queue:
-        score, row, col, direction = extractmin(queue)
-
-        dr, dc = directions[direction]
-        forward = (score + 1, row + dr, col + dc, direction, row + dr, col + dc)
-
-        rightdirection = (direction + 1) % len(directions)
-        dr, dc = directions[rightdirection]
-        right = (score + 1000, row, col, rightdirection, row + dr, col + dc)
-
-        leftdirection = (direction - 1) % len(directions)
-        dr, dc = directions[leftdirection]
-        left  = (score + 1000, row, col, leftdirection, row + dr, col + dc)
-
-        for score, row, col, direction, nextrow, nextcol in (forward, right, left):
-            if (place:=maze[row][col]) != "#" and maze[nextrow][nextcol] != "#":
-                addinqueue(queue, score, row, col, direction)
-            if place == "E":
-                bestscore = min(bestscore, score)
     print(bestscore)
-
-
 if aoc.part == "two":
-    maze = []
-    for row,line in enumerate(aoc.Input(split="")):
-        if "S" in line:
-            srow = row
-            scol = line.index("S")
-            line[scol] = "."
-        maze.append(line)
-
-    EAST = 0
-    SOUTH = 1
-    WEST = 2
-    NORTH = 3
-    directions = [(0,+1), (+1,0), (0,-1), (-1, 0)]
-
-    def extractmin(queue):
-        score = sys.maxsize
-        position = None
-        for k, v in queue.items():
-            if v[0] < score:
-                score = v[0]
-                position = k
-        score, tiles = queue.pop(position)
-        row, col, direction = position
-        return score, tiles, row, col, direction
-
-    pastpositions = {}
-    bestscore = sys.maxsize
-    besttiles = set()
-    def addinqueue(queue, score, tiles, row, col ,direction):  # noqa: PLR0913
-        global pastpositions, bestscore, besttiles
-        position = (row, col, direction)
-        s = pastpositions.get(position, sys.maxsize)
-        if s < score:
-            return
-        pastpositions[position] = score
-        position = (row, col ,direction)
-        previousscore, previoustiles = queue.get(position, (sys.maxsize, set()))
-        if previousscore < score:
-            return
-        if previousscore == score:
-            tiles = tiles | previoustiles
-        queue[position] = (score, tiles)
-        if maze[row][col] == "E":
-            if bestscore < score :
-                return
-            if bestscore == score:
-                tiles = tiles | besttiles
-            bestscore, besttiles = score, tiles
-
-
-    bs = sys.maxsize
-    queue = {(srow, scol, EAST): (0, {(srow,scol)})}
-    while queue:
-        score, tiles, row, col, direction = extractmin(queue)
-
-        dr, dc = directions[direction]
-        forward = (score + 1, tiles | {(row + dr, col + dc)}, row + dr, col + dc, direction, row + dr, col + dc)
-
-        rightdirection = (direction + 1) % len(directions)
-        dr, dc = directions[rightdirection]
-        right = (score + 1000, set(tiles), row, col, rightdirection, row + dr, col + dc)
-
-        leftdirection = (direction - 1) % len(directions)
-        dr, dc = directions[leftdirection]
-        left  = (score + 1000, set(tiles), row, col, leftdirection, row + dr, col + dc)
-
-        for score, tiles, row, col, direction, nextrow, nextcol in (forward, right, left):
-            if maze[row][col] != "#" and maze[nextrow][nextcol] != "#":
-                addinqueue(queue, score, tiles, row, col, direction)
-            if maze[row][col] == "E":
-                bs = min(bs, score)
-
-            #if maze[row][col] == "E":
-            #    print(score, bestscore)
-    print(bestscore, bs, len(besttiles))
-
-    #for r,c in besttiles:
-    #    maze[r][c] = "O"
-    #for line in maze:
-    #    print("".join(line))
-
-
-# 433 too low
+    print(len(besttiles))
