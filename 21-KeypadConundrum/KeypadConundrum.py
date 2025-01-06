6aa6e156e66299056ad0d0f64e139e3b0bd04a81
cie <edouard.hinard@laposte.net>
2025-01-06 22:28:13 +0100

    21**

--- 21-KeypadConundrum/KeypadConundrum.py
+++ 21-KeypadConundrum/KeypadConundrum.py
@@ -1,103 +1,64 @@
 #! /usr/bin/env python3
 
+import collections
+import functools
 import itertools
 
 import aoc
 
+#  Numeric Keypad
+#   +---+---+---+
+# 3 | 7 | 8 | 9 |
+#   +---+---+---+
+# 2 | 4 | 5 | 6 |   Directional Keypad
+#   +---+---+---+          +---+---+
+# 1 | 1 | 2 | 3 |    1   ! | ^ | * | <- activate button renamed /!\
+#   +---+---+---+      +---+---+---+
+# 0   ! | 0 | A |    0 | < | v | > |    "!" is the forbidden key above which the arm must not go
+#       +---+---+      +---+---+---+
+#     0   1   2          0   1   2
+Key = collections.namedtuple("Key", "r c")
+numerickeypad = {key: Key(r, c) for r, row in enumerate(("!0A", "123", "456", "789")) for c, key in enumerate(row)}
+directionalkeypad = {key: Key(r, c) for r, row in enumerate(("<v>", "!^*")) for c, key in enumerate(row)}
 
+def robotsequence(position1, position2):
+    # Returns one or two key sequence on a robot directional keypad in order to move his arm
+    # from position1 to position2 and press (both positions given as key symbols which determine the target keypad)
+    keypad = numerickeypad if position1 in "0123456789A" else directionalkeypad
+    dr = (r2 := keypad[position2].r) - (r1 := keypad[position1].r)
+    dc = (c2 := keypad[position2].c) - (c1 := keypad[position1].c)
+    updownsubsequence = "v^"[dr > 0] * abs(dr)
+    leftrightsubsequence = "<>"[dc > 0] * abs(dc)
+    if (r1, c2) == keypad["!"]:
+        # cannot start with left-right sub-sequence since it goes through forbidden key
+        return (updownsubsequence+leftrightsubsequence+"*",)
+    if (r2, c1) == keypad["!"]:
+        # cannot start with up-down sub-sequence since it goes through forbidden key
+        return (leftrightsubsequence+updownsubsequence+"*",)
+    # both sequence are allowed: left-right sub-sequence followed by up-down sub-sequence and the opposite,
+    #  but since they can be identical if one or the other is empty, a set is used to keep only one in that case
+    return tuple({leftrightsubsequence+updownsubsequence+"*", updownsubsequence+leftrightsubsequence+"*"})
+
+
+@functools.cache
+def lengthofminimalsequence(sequence, keypadindex):
+    # Returns the length of the shortest key sequence on keypad #keypadindex
+    #  that generates the input sequence on keypad #numberofkeypad
+    if keypadindex == numberofkeypad:
+        return len(sequence)
+    activatekey = "A" if keypadindex == 1 else "*"
+    res = 0
+    for position1, position2 in itertools.pairwise(activatekey + sequence):
+        res += min(lengthofminimalsequence(seq, keypadindex+1) for seq in robotsequence(position1, position2))
+    return res
 
 
 if aoc.part == "one":
-    #   +---+---+---+
-    # 3 | 7 | 8 | 9 |
-    #   +---+---+---+
-    # 2 | 4 | 5 | 6 |
-    #   +---+---+---+
-    # 1 | 1 | 2 | 3 |
-    #   +---+---+---+
-    # 0     | 0 | A |
-    #       +---+---+
-    #     0   1   2
-    def numpad(num, previous):
-        if num == previous:
-            return [""]
-        keypad = {"7": (0,3), "8": (1,3), "9": (2,3),
-                  "4": (0,2), "5": (1,2), "6": (2,2),
-                  "1": (0,1), "2": (1,1), "3": (2,1),
-                              "0": (1,0), "A": (2,0)}
-        dx = keypad[num][0] - keypad[previous][0]
-        dy = keypad[num][1] - keypad[previous][1]
-        hor = (">" if dx > 0 else "<") * abs(dx)
-        ver = ("^" if dy > 0 else "v") * abs(dy)
-        if previous in "0A" and num in "147":
-            return [ver+hor]
-        if previous in "147" and num in "0A":
-            return [hor+ver]
-        #return [hor+ver]
-        return list({hor+ver, ver+hor})
-
-    #       +---+---+
-    # 1     | ^ | A |
-    #   +---+---+---+
-    # 0 | < | v | > |
-    #   +---+---+---+
-    #     0   1   2
-    def dirpad(direction, previous):
-        if direction == previous:
-            return [""]
-        keypad = {            "^": (1,1), "A": (2,1),
-                  "<": (0,0), "v": (1,0), ">": (2,0)}
-        dx = keypad[direction][0] - keypad[previous][0]
-        dy = keypad[direction][1] - keypad[previous][1]
-        hor = (">" if dx > 0 else "<") * abs(dx)
-        ver = ("^" if dy > 0 else "v") * abs(dy)
-        if previous in "^A" and direction in ">":
-            return [ver+hor]
-        if previous in ">" and direction in "^A":
-            return [hor+ver]
-        #return [hor+ver]
-        return list({hor+ver, ver+hor})
-
-    count = 0
-    for nums in aoc.Input():
-        print()
-        print(nums)
-        import sys
-        minscore = sys.maxsize
-        for dirsequences1 in itertools.product(*(numpad(num, previous=previous) for previous, num in itertools.pairwise("A" + nums))):
-            dirsequences1 = "A".join(dirsequences1) + "A"  # noqa: PLW2901
-            #print(dirsequences1)
-
-            for dirsequences2 in itertools.product(*(dirpad(dir, previous=previous) for previous, dir in itertools.pairwise("A" + dirsequences1))):
-                dirsequences2 = "A".join(dirsequences2) + "A"  # noqa: PLW2901
-                #print(" " + dirsequences2)
-
-                for dirsequences3 in itertools.product(*(dirpad(dir, previous=previous) for previous, dir in itertools.pairwise("A" + dirsequences2))):
-                    dirsequences3 = "A".join(dirsequences3) + "A"  # noqa: PLW2901
-                    minscore = min(minscore, len(dirsequences3) * int(nums[:-1]))
-                    #print("  " + dirsequences3)
-                    #print(len(dirsequences3), int(nums[:-1]))
-                    #count += len(dirsequences3) * int(nums[:-1])
-        count += minscore
-    print(count)
-
-# 188192 too high
-
-
+    numberofkeypad = 4
 
 if aoc.part == "two":
-    NUMROBOT = 26
-    robots = [{"A": "A"} for _ in range(NUMROBOT)]
-
-    def findinput(output, robots=robots):
-        if not robots:
-            return output
-        if output not in robots[0]:
-            robots[output] = 
-        
-        return robots[0][output]
+    numberofkeypad = 27
 
 
-
-
-
+complexity = sum(lengthofminimalsequence(doorcode, 1) * int(doorcode[:-1]) for doorcode in aoc.Input())
+print(complexity)
